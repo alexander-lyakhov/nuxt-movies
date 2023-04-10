08f3845f7bfe13fe7b100c82aa080c4bf57316bf
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2023-04-10 23:55:10 +0300

    ...

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/styles/reset.css',
+    '@/assets/styles/main.scss',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -33,12 +36,17 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/style-resources',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'https://cinema-api-test.y-media.io/v1',
+  },
+
+  styleResources: {
+    scss: ['@/assets/styles/index.scss']
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
